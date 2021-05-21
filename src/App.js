209e77e12a1db95b0f25f2cec2e89a0c21f8a3ce
Vaishnavi Salve <vaishnavisalve655@gmail.com>
2021-05-21 09:48:50 +0530

    formated code with seperate file of footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Project from "./Project";
+import Footer from "./Footer";
 
 function App () {
 
@@ -10,6 +11,7 @@ function App () {
     <div className="App">
 
   <Project/>
+  <Footer/>
  
 </div>
   );
--- src/Project.js
+++ src/Project.js
@@ -1,115 +1,85 @@
-import React from 'react'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function project() {
-    return (
-        <React.Fragment>
-<body class = ".bg-light.bg-gradient">
-<h1 class = "container text-start mt-5 mb-5" >Projects</h1>
+  return (
+    <React.Fragment>
+      <body class=".bg-light.bg-gradient">
+        <h1 class="container text-start mt-5 mb-5">Projects</h1>
 
-<div class="container text-start "> 
-    <div class="row">
-      
+        <div class="container text-start ">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+              <h2 class="display-6 mt-5 mb -5 ">
+                <b>Project Name 02</b>
+              </h2>
+              <p class="   mt-5 mb-5 ">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolorum maxime quas sequi aspernatur culpa saepe consequatur
+                delectus, magni, labore ipsa voluptas ipsum porro dolorem minima
+                accusamus quibusdam. Adipisci, corporis facere.
+              </p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+              <img
+                src="https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp"
+                alt=" error"
+                class="mt-5 mb-5 px-5 py-5 float-end img-fluid  "
+              />
+            </div>
+          </div>
+        </div>
 
-      <div class="col-lg-6 col-md-6 col-12">
-        <h2 class="display-6 mt-5 mb -5 "><b>Project Name 02</b></h2>
-        <p class="   mt-5 mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum maxime quas sequi aspernatur culpa saepe consequatur delectus, magni, labore ipsa voluptas ipsum porro dolorem minima accusamus quibusdam. Adipisci, corporis facere.</p>
-    
-      </div>
-      <div class="col-lg-6 col-md-6 col-12">
-        <img src= "https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp" alt=" error" class="mt-5 mb-5 px-5 py-5 float-end img-fluid  "/> 
-      </div>
-    </div>
-  </div> 
+        <div class="container text-start">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+              <h2 class="display-6 mt-5 mb -5 ">
+                <b>Project Name 02</b>
+              </h2>
+              <p class=" mt-5 mb-5 ">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolorum maxime quas sequi aspernatur culpa saepe consequatur
+                delectus, magni, labore ipsa voluptas ipsum porro dolorem minima
+                accusamus quibusdam. Adipisci, corporis facere.
+              </p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+              <img
+                src="https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp"
+                alt=" error"
+                class="mt-5 mb-5 px-5 py-5 float-end img-fluid"
+              />
+            </div>
+          </div>
+        </div>
 
- 
-  <div class="container text-start"> 
-    <div class="row">
-      
+        <div class="container text-start">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+              <h2 class="display-6 mt-5 mb -5 ">
+                <b>Project Name 02</b>
+              </h2>
+              <p class=" mt-5 mb-5 ">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolorum maxime quas sequi aspernatur culpa saepe consequatur
+                delectus, magni, labore ipsa voluptas ipsum porro dolorem minima
+                accusamus quibusdam. Adipisci, corporis facere.
+              </p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+              <img
+                src="https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp"
+                alt=" error"
+                class="mt-5 mb-5 px-5 py-5 float-end img-fluid"
+              />
+            </div>
+          </div>
+        </div>
 
-      <div class="col-lg-6 col-md-6 col-12">
-        <h2 class="display-6 mt-5 mb -5 "><b>Project Name 02</b></h2>
-        <p class=" mt-5 mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum maxime quas sequi aspernatur culpa saepe consequatur delectus, magni, labore ipsa voluptas ipsum porro dolorem minima accusamus quibusdam. Adipisci, corporis facere.</p>
-    
-      </div>
-      <div class="col-lg-6 col-md-6 col-12">
-        <img src= "https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp" alt=" error" class="mt-5 mb-5 px-5 py-5 float-end img-fluid"/> 
-      </div>
-    </div>
-  </div> 
-
-  <div class="container text-start"> 
-    <div class="row">
-      
-
-      <div class="col-lg-6 col-md-6 col-12">
-        <h2 class="display-6 mt-5 mb -5 "><b>Project Name 02</b></h2>
-        <p class=" mt-5 mb-5 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum maxime quas sequi aspernatur culpa saepe consequatur delectus, magni, labore ipsa voluptas ipsum porro dolorem minima accusamus quibusdam. Adipisci, corporis facere.</p>
-    
-      </div>
-      <div class="col-lg-6 col-md-6 col-12">
-        <img src= "https://static.wixstatic.com/media/c837a6_871a4251d9874b0d8acfe66796caaaf8~mv2.jpg/v1/fill/w_442,h_298,al_c,q_80,usm_0.66_1.00_0.01/brendan-church-9F3sEV1-Iog-unsplash.webp" alt=" error" class="mt-5 mb-5 px-5 py-5 float-end img-fluid"/> 
-      </div>
-    </div>
-  </div> 
- 
-
- 
- <hr/>
-   {/* footer */}
-
- <footer class=" text-start ">
-  <div class="container-fluid text-start p-4">
-    <div class="row">
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0 px-lg-5">
-        <h5>Phone no.</h5>
-        <ul class="list-unstyled  mb-0">
-          <li>
-            <a href="#!" class="text-dark text-decoration-none">123-456-789</a>
-          </li>
-        </ul>
-      </div> 
-      
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0  px-lg-5">
-        <h5 >E-mail</h5>
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!" class="text-dark text-decoration-none">info@gmail.com</a>
-          </li>
-        </ul>
-      </div>
-      
-    
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0  px-lg-5">
-        <h5>Follow Me</h5>
-        <ul class="list-unstyled  mb-0">
-        <li>  
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-</svg>
-          </li>
-        </ul>
-      </div>
-      
-      <div class="col-lg-3 col-md-6 mb-4 mb-md-0  px-lg-5">
-      <ul class="list-unstyled  mb-0">
-      <li>
-            <a href="#!" class="text-dark text-decoration-none ">© 2023 By Nicol Rider.<br/> Proudly created with Wix.com</a>
-          </li>
-          </ul>
-      </div>
-    </div> 
-  </div>
-</footer> 
-
-   
-</body>
-  </React.Fragment>
-    )
-    
+        <hr />
+      </body>
+    </React.Fragment>
+  );
 }
 
-export default project
+export default project;
