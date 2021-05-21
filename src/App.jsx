79029098bbf8386855f5394ff4584dfe4d7d2535
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-05-21 11:07:50 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Header"
-import Contact from "./Contact";
-import Footer from "./Footer"
+import Header from "./Components/Header"
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer"
 
 function App() {
 
